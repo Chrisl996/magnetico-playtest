5d06d51ea2a2973cda038fb531bf44595d6a7cf4
Chrisl996 <chris.j.lane996@gmail.com>
2026-07-29 16:21:15 +0100

    Updated for the day

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785321517|3778815398';
+const CACHE_VERSION = '1785338429|2178625336';
 /** @type {string} */
-const CACHE_PREFIX = 'Learning - Calm -sw-cache-';
+const CACHE_PREFIX = 'Magnetico - Comb-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
