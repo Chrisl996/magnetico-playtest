930b2669deb3e9bfd7d4923d5b4685a24458dfb9
Chrisl996 <chris.j.lane996@gmail.com>
2026-07-29 11:38:59 +0100

    Updated to add swipe and zoomed camera

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785320477|2739398825';
+const CACHE_VERSION = '1785321517|3778815398';
 /** @type {string} */
 const CACHE_PREFIX = 'Learning - Calm -sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
