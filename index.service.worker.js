f5e8b4f03de1d32ce0dc312ed3e1c74a6389e26a
Chrisl996 <chris.j.lane996@gmail.com>
2026-07-30 17:06:51 +0100

    Made big changes to how it plays today. Stripped a lot back

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785401937|1941602722';
+const CACHE_VERSION = '1785427592|291787554';
 /** @type {string} */
 const CACHE_PREFIX = 'Magnetico - Comb-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
