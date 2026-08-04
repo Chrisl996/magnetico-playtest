5c02f47596ca50a416f60f15392e67ad5483deca
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-04 11:39:46 +0100

    Updated MoveControls

--- docs/latest/index.service.worker.js
+++ docs/latest/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785774565|29036558881';
+const CACHE_VERSION = '1785839970|224808239';
 /** @type {string} */
 const CACHE_PREFIX = 'Magnetico - Prot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
