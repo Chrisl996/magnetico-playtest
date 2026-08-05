4274f4ee8baf46f0dbc4ed7f628ee27810fd224c
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-05 11:53:55 +0100

    chanegd offset

--- docs/latest/index.service.worker.js
+++ docs/latest/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785927166|9736294010';
+const CACHE_VERSION = '1785927226|9796198431';
 /** @type {string} */
 const CACHE_PREFIX = 'Magnetico - Prot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
