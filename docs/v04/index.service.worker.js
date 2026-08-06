80cbe03b1c52bc5ef7fbc1ff7930d838dafeaec1
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-06 09:44:53 +0100

    update blackholes mobile

--- docs/v04/index.service.worker.js
+++ docs/v04/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785945321|27890966653';
+const CACHE_VERSION = '1786005685|2044584459';
 /** @type {string} */
 const CACHE_PREFIX = 'Magnetico - Prot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
