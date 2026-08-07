94beb0e85fe8b1d82a2603e1f02f7848b363d564
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-07 09:54:54 +0100

    update mobile ratio

--- docs/latest/index.service.worker.js
+++ docs/latest/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786092627|2334187562';
+const CACHE_VERSION = '1786092885|2592185527';
 /** @type {string} */
 const CACHE_PREFIX = 'Magnetico - Prot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
