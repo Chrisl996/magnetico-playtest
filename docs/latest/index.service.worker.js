45fdeb388cb0d0189555c602bcf1fcebb0e530c0
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-07 16:49:44 +0100

    up

--- docs/latest/index.service.worker.js
+++ docs/latest/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786117007|4689375778';
+const CACHE_VERSION = '1786117768|5449933426';
 /** @type {string} */
 const CACHE_PREFIX = 'Magnetico - Prot-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
