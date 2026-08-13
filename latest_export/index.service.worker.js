41cab86750c6d3c69bc8bcbd0d8373cfbcdb6375
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-13 11:47:19 +0100

    Publish 13 August mobile playtest build

--- latest_export/index.service.worker.js
+++ latest_export/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786615036|7146845';
+const CACHE_VERSION = '1786617996|6861623';
 /** @type {string} */
 const CACHE_PREFIX = 'Gluon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
