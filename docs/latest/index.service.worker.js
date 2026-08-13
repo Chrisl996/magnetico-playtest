9e04adad484e8d4931b41a51de12b1a70032b19a
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-13 11:03:44 +0100

    Publish latest web build with timestamp

--- docs/latest/index.service.worker.js
+++ docs/latest/index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786463717|24740541673';
+const CACHE_VERSION = '1786615036|7146845';
 /** @type {string} */
-const CACHE_PREFIX = 'Magnetico - Prot-sw-cache-';
+const CACHE_PREFIX = 'Gluon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
