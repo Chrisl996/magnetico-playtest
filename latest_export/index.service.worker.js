524ae5020b8bb38fc5d2775b18aa5a69e324d97e
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-14 11:49:46 +0100

    Improve mobile controls and Level 2 completion

--- latest_export/index.service.worker.js
+++ latest_export/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786703477|6489314';
+const CACHE_VERSION = '1786704529|6247059';
 /** @type {string} */
 const CACHE_PREFIX = 'Gluon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
