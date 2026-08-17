801057c9bebee7d9c92edb837eb93a163c2d29c9
Chrisl996 <chris.j.lane996@gmail.com>
2026-08-17 11:55:37 +0100

    Update latest Godot playtest build

--- docs/latest/index.service.worker.js
+++ docs/latest/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786727146|7020905';
+const CACHE_VERSION = '1786964009|7016230';
 /** @type {string} */
 const CACHE_PREFIX = 'Gluon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
